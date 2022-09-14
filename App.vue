edd0410a042b3677e816406c6427aaf88e45f242
Ejing (Yijing) Liu <Ejing.Liu@monolithicpower.com>
2022-09-14 16:48:41 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- 快捷键：<+v 就可以直接出来模版 -->
+  <div>
+    <img src="./assets/logo.png" alt="logo" />
+    <!-- 3.使用组件 -->
+    <SchoolName></SchoolName>
+    <StudentName></StudentName>
+  </div>
+</template>
+
+<script>
+// 1.引入组件
+import SchoolName from "./components/SchoolName.vue";
+import StudentName from "./components/StudentName.vue";
+export default {
+  name: "App",
+  // 2.注册组件
+  components: {
+    StudentName,
+    SchoolName,
+  },
+};
+</script>
+
+<style></style>
---
+++ components/SchoolName.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学校名称：{{ schoolName }}</h2>
+    <h2>学校名称：{{ schoolAddress }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+// 组件交互的代码,采用默认暴露。const school 和 Vue.extend可以不写
+// const school = Vue.extend({
+export default {
+  name: "SchoolName",
+  data() {
+    return {
+      schoolName: "家里蹲大学",
+      schoolAddress: "哔站",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.schoolName);
+    },
+  },
+};
+</script>
+
+<style>
+/* 组件的样式 */
+.demo {
+  background: skyblue;
+}
+</style>

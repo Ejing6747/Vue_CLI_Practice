0dd0133305e3501ba96695658525f1051f75875b
Ejing6747 <113498134+Ejing6747@users.noreply.github.com>
2022-09-21 17:13:41 +0800

    Add files via upload

---
+++ 20_src_github案例/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <Search />
+    <List />
+  </div>
+</template>
+
+<script>
+//引入
+import Search from "./components/Search";
+import List from "./components/List";
+export default {
+  name: "App",
+  //注册
+  components: { Search, List },
+};
+</script>
